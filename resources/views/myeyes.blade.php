a0405bce40502925b174127805ea6f73163c3b15
n0o0b090lv <ernestspursw@gmail.com>
2025-10-29 22:58:20 +0200

    Call me crazy but maybe :DD

--- resources/views/myeyes.blade.php
+++ resources/views/myeyes.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    @vite('resources\css\app.css')
+    @vite('resources/css/app.css')
     <link rel="stylesheet" href="css/style.css" />
 </head>
 <body class="flex flex-col bg-black text-white p-10">
